5036348f8c2f5eb312515c0d2d948715fe5c5d72
Raja Kumar <56065083+rajak7549@users.noreply.github.com>
2024-01-14 23:42:37 +0530

    Add files via upload

---
+++ netspeeed.py
@@ -0,0 +1,9 @@
+import speedtest
+speed_test=speedtest.Speedtest()
+
+def bytes_to_mb(bytes):
+    KB=1024
+    MB=KB*1024
+    return int (bytes/MB)
+download_speed=bytes_to_mb(speed_test.download())
+print("your download speed is ",download_speed,"MB")
